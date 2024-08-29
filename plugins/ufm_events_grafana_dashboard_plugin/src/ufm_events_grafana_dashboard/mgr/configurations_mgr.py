06544c3a659b8c1418280bc6acb5185ca62638e2
ananalaghbar <anana@nvidia.com>
2024-08-29 08:48:08 +0100

    issue: 4040981: [UFM Events Grafana Dashboard Plugin]: Initial integration between Netfix's link down algorithm with Grafana plugin
    - issue:4040984: [UFM Events Grafana Dashboard Plugin]: Generate CSV Report that gives online summary about the links flapping status
    
    - issue: 4049851: [UFM Events Grafana Dashboard Plugin]: Add new Prometheus metric for link_flapping_counter
    
    ++ general fixes on the UFM events logs parsing

--- plugins/ufm_events_grafana_dashboard_plugin/src/ufm_events_grafana_dashboard/mgr/configurations_mgr.py
+++ plugins/ufm_events_grafana_dashboard_plugin/src/ufm_events_grafana_dashboard/mgr/configurations_mgr.py
@@ -9,6 +9,8 @@
 # This software product is governed by the End User License Agreement
 # provided with the software product.
 #
+import re
+import os
 from typing import List
 
 from utils.config_parser import ConfigParser
@@ -33,6 +35,10 @@ class UFMEventsGrafanaConfigParser(ConfigParser, Singleton):
     PROMETHEUS_SECTION_IP = "prometheus_ip"
     PROMETHEUS_SECTION_PORT = "prometheus_port"
     PROMETHEUS_REMOTE_WRITE_MAX_CHUNK_SIZE = "prometheus_remote_write_max_chunk_size"
+    ############
+    FLAPPING_LINKS_CONFIG_SECTION = 'flapping-links'
+    FLAPPING_LINKS_INTERVAL = 'telemetry_sample_interval'
+    FLAPPING_LINKS_OUTPUT_PATH = 'output_path'
 
     def __init__(self):
         super().__init__(read_sdk_config=False)
@@ -84,3 +90,37 @@ class UFMEventsGrafanaConfigParser(ConfigParser, Singleton):
         return self.safe_get_int(None,
                                  self.PROMETHEUS_CONFIG_SECTION,
                                  self.PROMETHEUS_REMOTE_WRITE_MAX_CHUNK_SIZE, 10000)
+
+    #####################################
+
+    def get_links_flapping_output_path(self) -> str:
+        _path = self.get_config_value(None,
+                                      self.FLAPPING_LINKS_CONFIG_SECTION,
+                                      self.FLAPPING_LINKS_OUTPUT_PATH, "/config/link_flipping")
+        os.makedirs(_path, exist_ok=True)
+        return _path
+
+    def get_links_flapping_interval_in_secs(self) -> int:
+        duration = self.get_config_value(None,
+                                         self.FLAPPING_LINKS_CONFIG_SECTION,
+                                         self.FLAPPING_LINKS_INTERVAL,
+                                         "2h")
+        # regex pattern to match the duration format <number>d|h|m
+        pattern = r'^(\d+)([dhm])$'
+
+        match = re.match(pattern, duration)
+
+        if not match:
+            raise ValueError(f"Invalid format. The [{self.FLAPPING_LINKS_CONFIG_SECTION}.{self.FLAPPING_LINKS_INTERVAL}] "
+                             f"must be in the format '<number>d|h|m'.")
+
+        number = int(match.group(1))
+        unit = match.group(2)
+
+        # Convert to seconds based on the unit
+        if unit == 'd':
+            return number * 24 * 60 * 60  # days to seconds
+        elif unit == 'h':
+            return number * 60 * 60  # hours to seconds
+        elif unit == 'm':
+            return number * 60  # minutes to seconds
